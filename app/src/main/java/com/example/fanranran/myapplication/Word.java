83821b75a044fe3fe8c912ee0c247411a84736b5
fanranran <ran.fan@tricubics.com>
2016-08-24 11:42:12 +0900

    new class

---
+++ app/src/main/java/com/example/fanranran/myapplication/Word.java
@@ -0,0 +1,16 @@
+package com.example.fanranran.myapplication;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by fanranran on 16. 8. 24..
+ */
+public class Word extends AppCompatActivity{
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_main);
+    }
+}
