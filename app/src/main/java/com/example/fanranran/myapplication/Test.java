c7c122a93cffa319616d276666fed6ce084b30bc
FANRANRAN <ran.fan@tricubics.com>
2016-08-25 12:55:59 +0900

    Test

--- app/src/main/java/com/example/fanranran/myapplication/Test.java
+++ app/src/main/java/com/example/fanranran/myapplication/Test.java
@@ -1,7 +1,17 @@
 package com.example.fanranran.myapplication;
 
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by fanranran on 16. 8. 24..
  */
-public class Test {
+public class Test extends AppCompatActivity{
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+        setContentView(R.layout.activity_main);
+    }
 }
