356983e7edb3def07cf6389150bcde023d6af7cc
FANRANRAN <ran.fan@tricubics.com>
2016-08-25 11:40:18 +0900

    Second

--- app/src/main/java/com/example/fanranran/myapplication/MainActivity.java
+++ app/src/main/java/com/example/fanranran/myapplication/MainActivity.java
@@ -7,16 +7,27 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    TextView textView;
+    TextView textView,textView02;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textView=(TextView) findViewById(R.id.textView);
+        textView02=(TextView)findViewById(R.id.textView02);
+
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(getApplicationContext(),"버튼 놀어요!",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        textView02.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+Toast.makeText(getApplicationContext(), "blabla", Toast.LENGTH_SHORT).show();
+
             }
         });
 
