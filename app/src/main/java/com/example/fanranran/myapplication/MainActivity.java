43b6c52fb033994a93a24d4216bb03c9734c6770
FANRANRAN <ran.fan@tricubics.com>
2016-08-25 10:08:46 +0900

    first commit

--- app/src/main/java/com/example/fanranran/myapplication/MainActivity.java
+++ app/src/main/java/com/example/fanranran/myapplication/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.fanranran.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"버튼 놀어요!",Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
+
 }
