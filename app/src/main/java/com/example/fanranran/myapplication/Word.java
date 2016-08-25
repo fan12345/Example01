b9b0a88c56c385da8b3a013a28358458821a63e0
FANRANRAN <ran.fan@tricubics.com>
2016-08-25 11:48:27 +0900

    Word revise

--- app/src/main/java/com/example/fanranran/myapplication/Word.java
+++ app/src/main/java/com/example/fanranran/myapplication/Word.java
@@ -3,14 +3,27 @@ package com.example.fanranran.myapplication;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * Created by fanranran on 16. 8. 24..
  */
 public class Word extends AppCompatActivity{
+
+    Button btn;
     @Override
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
         setContentView(R.layout.activity_main);
+        btn=(Button)findViewById(R.id.button01);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"11111",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
